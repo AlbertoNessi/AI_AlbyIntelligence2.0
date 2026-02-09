851d28ab2df35d288fccf07bd941f3e30d92291a
alberto.nessi <albertonessi1@gmail.com>
2026-02-09 21:35:07 +0100

    First Employess and Contracts models

--- alby_intelligence/models.py
+++ alby_intelligence/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Contracts(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200)
+
+
+class Employees(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    address_number = models.CharField(max_length=5)
+    birthday = models.DateField("Date of birth")
+    place_of_birth = models.CharField(max_length=100)
+    fiscal_code = models.CharField(max_length=11)
+    contract = models.ForeignKey(Contracts, on_delete=models.CASCADE)
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'alby_intelligence.apps.AlbyIntelligenceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,6 +81,7 @@ DATABASES = {
         'PASSWORD': 'django_pass_123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+                'TIME_ZONE': 'Europe/Rome'
     }
 }
 
